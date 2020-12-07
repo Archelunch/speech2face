36777d3c67aca69a5d97b9b5a89099454745fc1a
Archelunch <alivemihail@gmail.com>
2020-12-07 22:42:52 +0300

    last

--- train.py
+++ train.py
@@ -209,7 +209,7 @@ def main():
         checkpoint_callback=checkpoint_callback,
         accumulate_grad_batches=accumulate_grad_batches,
         val_check_interval=0.2,
-        #resume_from_checkpoint=saved_checkpoint,
+        resume_from_checkpoint=saved_checkpoint,
         auto_select_gpus=True,
     )
     trainer.fit(glow_light)
